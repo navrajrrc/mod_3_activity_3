bcb86f88c6bf76314fe85fa190b64806c0679457
Navraj Singh <navrajsingh@rrc.ca>
2025-03-07 12:12:29 -0600

    Done the class payment

--- payment/payment.py
+++ payment/payment.py
@@ -1,7 +1,24 @@
 """This module defines the Payment class."""
 
-__author__ = ""
+__author__ = "NAVRAJ SINGH"
 __version__ = ""
 
 from billing_account.billing_account import BillingAccount
 from payee.payee import Payee
+from patterns.strategy.payment_strategy import PaymentStrategy
+
+class Payment:
+    def __init__(self, strategy: PaymentStrategy):
+        """
+        
+        """
+        if isinstance(strategy, PaymentStrategy):
+            self.__strategy = strategy
+        else: 
+            raise ValueError("Invalid Strategy")
+        
+    def pay_bill(self,account:BillingAccount, payee: Payee, amount: float)-> str:
+        """
+            
+        """
+        return self.__strategy.process_payment(account, payee, amount)
