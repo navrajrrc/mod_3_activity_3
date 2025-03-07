2ee3ddebba5f8f4e9b7b755018c6c25f16e72a28
Navraj Singh <navrajsingh@rrc.ca>
2025-03-07 11:40:21 -0600

    Done the payment_strategy class

--- patterns/strategy/payment_strategy.py
+++ patterns/strategy/payment_strategy.py
@@ -1,4 +1,21 @@
 """This module defines the PaymentStrategy class."""
 
-__author__ = ""
+__author__ = "Navraj singh"
 __version__ = ""
+
+from abc import ABC, abstractmethod 
+
+class PaymentStrategy(ABC):
+    """
+    
+    """
+    @abstractmethod
+    def process_payment(self, account, payee, amount) -> str:
+        """
+        This will process a payment from the given account
+        Account: Billing account
+        Payee: payee
+        amount: float
+        Returns: String
+        """
+        pass 
